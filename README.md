858752e02c0f8b852851c2887208fd8e61c3e250
entbappy <entbappy73@gmail.com>
2023-08-26 12:15:07 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Yolov5-app-deployment-on-AWS
+# YOLO web app
+
+
+315865595366.dkr.ecr.us-east-1.amazonaws.com/yoloapp
+
---
+++ signLanguage/utils/main_utils.py
@@ -0,0 +1,16 @@
+import os.path
+import sys
+import yaml
+import base64
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
